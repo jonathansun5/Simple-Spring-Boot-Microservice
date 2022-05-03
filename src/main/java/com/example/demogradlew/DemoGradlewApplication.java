d0e4166e84c56baae4d2c7e61cac80813bd11569
Jonathan Sun <jonathansun5@berkeley.edu>
2022-05-02 23:45:33 -0700

    completed

---
+++ src/main/java/com/example/demogradlew/DemoGradlewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogradlew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradlewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGradlewApplication.class, args);
+    }
+
+}
